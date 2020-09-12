1765651a3a3317dc17051d98737c44199b1829d9
BabyDriver2018 <bryan.mendoza@unas.edu.pe>
2020-09-11 20:59:27 -0500

    notification complet V1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Init;
 class HomeController extends Controller
 {
     /**
@@ -23,7 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('adminComponent.index');
+
+        return view('adminComponent.index',['message'=>Init::index()]);
     }
     
 }
--- app/Init.php
+++ app/Init.php
@@ -9,15 +9,18 @@ class Init extends Model
 {
     //
     public static function index(){
-        $stockProd = Products::all()->where('stock','<=',5);
-        if(empty($stockProd)){
-            //cuando no esta vacio
-            dd("no vacio");
+        //buscamos los productos q tienen menores a 5
+        $stockProdinAlmacen = Products::all()->where('stock','<=',5);
+        if(!empty($stockProdinAlmacen)){
+            //cuando no esta vaci
+            //dd($stockProdinAlmacen);
+            return 'Productos Agotandose menos de 5!';
         }
         else{
             //cuando esta vacio
-            dd($stockProd);
+            //dd($stockProdinAlmacen);
+            return 'Por el momento todo esta en orden!';
         }
-        return ;
+        
     }
 }
